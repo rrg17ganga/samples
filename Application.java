f4e52c4ad4887a08cabb1b43f6b07dd4a6b8f21f
rrg17ganga <89194685+rrg17ganga@users.noreply.github.com>
2021-08-23 19:59:58 +0530

    Update Application.java

--- Application.java
+++ Application.java
@@ -3,14 +3,14 @@ package samples;
 
 	public class Application {
 		public static void main(String[]  args) {
-			int a=50;
+			int a=50; // initialization
 			
-		//boolean  c=a<b;
+		//boolean  c=a<b;    
 		//System.out.println(c);
 			while(a<=52)
 			{
 				System.out.println("number less than 53");
-				a= a+2;
+				a= a+2; // increment a value by 2 after execution
 				
 			}
 		}
