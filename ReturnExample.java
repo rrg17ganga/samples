01b3edd4d802146618f66b66f345b821b6121084
rrg17ganga <89194685+rrg17ganga@users.noreply.github.com>
2021-08-23 20:08:23 +0530

    Add files via upload

---
+++ ReturnExample.java
@@ -0,0 +1,17 @@
+ //Return Example
+public class ReturnExample {
+		  static int myMethod(int a) {
+			 
+		  return 4+ a;//  a value will be added by 4
+		  }
+
+		  public static void main(String[] args) 
+		  {
+		 //myMethod(4);// does not display anything
+		  
+		    System.out.println(myMethod(5));//  4+5  so the result will be 9
+		  }
+		}
+
+
+
